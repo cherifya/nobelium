37dbc91c56691f2a1e5f543ca0195c326990021f
Cherif Yaya <chef.ya@gmail.com>
2025-11-02 01:37:27 -0700

    Minor updates to config

--- blog.config.js
+++ blog.config.js
@@ -11,14 +11,14 @@ const BLOG = {
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
-  since: 2021, // If leave this empty, current year will be used.
+  since: 2018, // If leave this empty, current year will be used.
   postsPerPage: 100,
-  sortByDate: false,
+  sortByDate: true,
   showAbout: true,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://twitter.com/cherifyaya',
+  socialLink: 'https://twitter.com/cherifya',
   socialLinks: {
     twitter: 'https://twitter.com/allad',
     github: 'https://github.com/cherifya',
